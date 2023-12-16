3466360a09be912d6034727cbb3e4aaffcf564c6
Debora Silva <deboraro.61@gmail.com>
2023-12-15 21:06:27 -0300

    0105 JWT

--- wp-config.php
+++ wp-config.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * As configurações básicas do WordPress
  *
@@ -20,22 +21,22 @@
 
 // ** Configurações do banco de dados - Você pode pegar estas informações com o serviço de hospedagem ** //
 /** O nome do banco de dados do WordPress */
-define( 'DB_NAME', 'api_wp' );
+define('DB_NAME', 'api_wp');
 
 /** Usuário do banco de dados MySQL */
-define( 'DB_USER', 'root' );
+define('DB_USER', 'root');
 
 /** Senha do banco de dados MySQL */
-define( 'DB_PASSWORD', '' );
+define('DB_PASSWORD', '');
 
 /** Nome do host do MySQL */
-define( 'DB_HOST', 'localhost' );
+define('DB_HOST', 'localhost');
 
 /** Charset do banco de dados a ser usado na criação das tabelas. */
-define( 'DB_CHARSET', 'utf8mb4' );
+define('DB_CHARSET', 'utf8mb4');
 
 /** O tipo de Collate do banco de dados. Não altere isso se tiver dúvidas. */
-define( 'DB_COLLATE', '' );
+define('DB_COLLATE', '');
 
 /**#@+
  * Chaves únicas de autenticação e salts.
@@ -50,14 +51,16 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'UA_ RDghZ8f2q$^WK0rF}&lJR@nc`sX!Ho+.X5us([I7WG,292*(Z?,r%)^ZhS$b' );
-define( 'SECURE_AUTH_KEY',  '[tvx}6jm{1ZWPNE})jK&]^$:_Y7g bOs/hBIU>(NMIadg]6X]b|4 }FxXU[flAyU' );
-define( 'LOGGED_IN_KEY',    'f,m|om~qy#&c><?OidJ7TL_{fgM+xzZ?j>Yf^#gAZM35`6-xeW%R)#AVRY9y( ~z' );
-define( 'NONCE_KEY',        'bm:C[7bF9=5;2H3sW>zt#GecmI%vkcry5lv<Mt$*kJ8>tO|R.N<$KS2jMu*f^{%>' );
-define( 'AUTH_SALT',        '@!ckN>Efx#1>+%XpOQ(x}L;<(yb@yA)p#kQKGBZ06mANa</^S*va;>k}(LmiBjtI' );
-define( 'SECURE_AUTH_SALT', 'i?j>`z&>M]42[]y97_{/b3dv<:zqym f-Ii[{mq>o+j#=f3fVp4-rH[g:ery/x/x' );
-define( 'LOGGED_IN_SALT',   'TW7/Bv<Do/4=6[?/tkxx#{./C!f+4Y;w%C*,DL[Te$:.{II&U5PR<kjp5&.COZ(K' );
-define( 'NONCE_SALT',       ')#^2L5/o!(nt!jie#v[0Fuq^hZhY&L9)n7b3p0K7b4>Wim|UC*9t=0DX/*1[VSDv' );
+define('AUTH_KEY',         'UA_ RDghZ8f2q$^WK0rF}&lJR@nc`sX!Ho+.X5us([I7WG,292*(Z?,r%)^ZhS$b');
+define('SECURE_AUTH_KEY',  '[tvx}6jm{1ZWPNE})jK&]^$:_Y7g bOs/hBIU>(NMIadg]6X]b|4 }FxXU[flAyU');
+define('LOGGED_IN_KEY',    'f,m|om~qy#&c><?OidJ7TL_{fgM+xzZ?j>Yf^#gAZM35`6-xeW%R)#AVRY9y( ~z');
+define('NONCE_KEY',        'bm:C[7bF9=5;2H3sW>zt#GecmI%vkcry5lv<Mt$*kJ8>tO|R.N<$KS2jMu*f^{%>');
+define('AUTH_SALT',        '@!ckN>Efx#1>+%XpOQ(x}L;<(yb@yA)p#kQKGBZ06mANa</^S*va;>k}(LmiBjtI');
+define('SECURE_AUTH_SALT', 'i?j>`z&>M]42[]y97_{/b3dv<:zqym f-Ii[{mq>o+j#=f3fVp4-rH[g:ery/x/x');
+define('LOGGED_IN_SALT',   'TW7/Bv<Do/4=6[?/tkxx#{./C!f+4Y;w%C*,DL[Te$:.{II&U5PR<kjp5&.COZ(K');
+define('NONCE_SALT',       ')#^2L5/o!(nt!jie#v[0Fuq^hZhY&L9)n7b3p0K7b4>Wim|UC*9t=0DX/*1[VSDv');
+define('JWT_AUTH_SECRET_KEY', 'Rc+x2qU]KuGHq-8 q$+A#=,D)p?^5bvHzXr8+;hs[,~[h-0a,,,}7n|Ck|aJeE6f');
+define('JWT_AUTH_CORS_ENABLE', true);
 
 /**#@-*/
 
@@ -82,7 +85,7 @@ $table_prefix = 'wp_';
  *
  * @link https://wordpress.org/documentation/article/debugging-in-wordpress/
  */
-define( 'WP_DEBUG', false );
+define('WP_DEBUG', false);
 
 /* Adicione valores personalizados entre esta linha até "Isto é tudo". */
 
@@ -91,8 +94,8 @@ define( 'WP_DEBUG', false );
 /* Isto é tudo, pode parar de editar! :) */
 
 /** Caminho absoluto para o diretório WordPress. */
-if ( ! defined( 'ABSPATH' ) ) {
-	define( 'ABSPATH', __DIR__ . '/' );
+if (!defined('ABSPATH')) {
+	define('ABSPATH', __DIR__ . '/');
 }
 
 /** Configura as variáveis e arquivos do WordPress. */
--- wp-content/themes/api/functions.php
+++ wp-content/themes/api/functions.php
@@ -6,3 +6,11 @@ $template_directory = get_template_directory();
 require_once($template_directory . '/custom-post-type/produto.php');
 require_once($template_directory . '/custom-post-type/transacao.php');
 require_once($template_directory . '/endpoints/usuario_post.php');
+require_once($template_directory . '/endpoints/usuario_get.php');
+
+function expire_token()
+{
+  return time() + (60 * 60 * 24);
+}
+
+add_action('jwt_auth_expire', 'expire_token');
